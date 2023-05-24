d679b158c53b611a48fd401e8085470f5e681ec8
Muhammad Yousuf <115397597+YousufRajput@users.noreply.github.com>
2023-05-24 12:02:05 +0500

    Add files via upload

---
+++ Odd&Even.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Yousuf{
+
+    public static void main(String args[]){
+      
+
+       System.out.println("Enter the number");
+       
+       Scanner s=new Scanner(system.in);
+       s= nextint();
+       
+       if(a%==0)
+       {
+           system.out.println("Number is even");
+           else
+{
+    system.out.println("Number is odd");
+}
+       }
+    }
+}
